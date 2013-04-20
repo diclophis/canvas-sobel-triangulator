876ea646a863ec49426cf39c5fcfecd7c74b3590
Diclophis <diclophis@gmail.com>
2013-04-20 03:05:50 -0700

    cleanup rendering

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -18,10 +18,10 @@ var canvas = document.createElement('canvas');
 var context = canvas.getContext('2d');
 var canvasOut = document.createElement('canvas');
 var contextOut = canvasOut.getContext('2d');
+var canvasTri = document.createElement('canvas');
+var contextTri = canvasTri.getContext('2d');
 
 var sortFuncCenteredAt = function(center) {
-
-//var center = {x: 72, y: 72};
 
   var fixBool = function(b) {
   return 0;
@@ -33,8 +33,6 @@ var sortFuncCenteredAt = function(center) {
   };
 
   var sortClockwise = function(a, b) {
-
-
     if (a.x >= 0 && b.x < 0) {
       return fixBool(true);
     }
@@ -80,29 +78,6 @@ var sortFuncCenteredAt = function(center) {
   return sortClockwise2;
 };
 
-/*
-var get_polygon_centroid = function(pts) {
-   var twicearea=0,
-       x=0, y=0,
-       nPts = pts.length,
-       p1, p2, f;
-
-   for (var i=0, j=nPts-1; i<nPts; j=i++) {
-      p1 = pts[i]; p2 = pts[j];
-      twicearea += p1.x * p2.y;
-console.log(twicearea);
-      twicearea -= p1.y * p2.x;
-console.log(twicearea, p1, p2);
-      f = p1.x * p2.y - p2.x * p1.y;
-      x += (p1.x + p2.x) * f;
-      y += (p1.y + p2.y) * f;
-   }
-   f = twicearea * 3;
-console.log(x, y, f, twicearea);
-   return {x: x / f, y: y / f};
-}
-*/
-
 var get_polygon_centroid = function(points) {  
   var centroid = {x: 0, y: 0};
   for(var i = 0; i < points.length; i++) {
@@ -129,10 +104,8 @@ var mergeVertices = function(vertices) {
     if (verticesMap[key] === undefined) {
       verticesMap[key] = i;
       unique.push(vertices[i]);
-      //changes[ i ] = unique.length - 1;
     } else {
       //console.log('Duplicate vertex found. ', i, ' could be using ', verticesMap[key], v);
-      //changes[ i ] = changes[ verticesMap[ key ] ];
     }
   }
 
@@ -155,8 +128,6 @@ function handleTestClick()
 
   var centroid = get_polygon_centroid(points);
 
-  console.log(centroid);
-
   var sortFunc = sortFuncCenteredAt(centroid);
 
   points.sort(sortFunc);
@@ -169,11 +140,14 @@ function handleTestClick()
 
   simplifiedPoints = simplify(points, 1, true);
 
-  console.log(simplifiedPoints.length);
+  console.log("simplified", simplifiedPoints.length);
+
+  simplifiedMergedPoints = mergeVertices(simplifiedPoints);
+
+  console.log("simplifiedMerged", simplifiedMergedPoints.length);
   
-  var grayPixels = A2RGBA(bwPixelsSobolev, context);
-  
-  contextOut.putImageData(grayPixels, 0,0);
+  //var grayPixels = A2RGBA(bwPixelsSobolev, context);
+  //contextOut.putImageData(grayPixels, 0,0);
 
   contextOut.fillStyle = 'yellow';
   contextOut.fillRect(centroid.x, centroid.y, 1, 1);
@@ -188,29 +162,20 @@ function handleTestClick()
     contextOut.fillRect(mergedPoints[i].x, mergedPoints[i].y, 1, 1);
   }
 
-  //var contour = new Array();
-
   contextOut.fillStyle = 'red';
   for (var i=0; i<simplifiedPoints.length; i++) {
-    //console.log(simplifiedPoints[i]);
-    //contextOut.arc(simplifiedPoints[i].x, simplifiedPoints[i].y, 10, 0, 2 * Math.PI);
     contextOut.fillRect(simplifiedPoints[i].x, simplifiedPoints[i].y, 2, 2);
-    //contour.push(new poly2tri.Point(simplifiedPoints[i].x, simplifiedPoints[i].y));
   }
 
+  contextOut.fillStyle = 'blue';
+  for (var i=0; i<simplifiedMergedPoints.length; i++) {
+    contextOut.fillRect(simplifiedMergedPoints[i].x, simplifiedMergedPoints[i].y, 2, 2);
+  }
 
-  //var contour = [
-  //  new poly2tri.Point(100, 100),
-  //  new poly2tri.Point(100, 300),
-  //  new poly2tri.Point(300, 300),
-  //  new poly2tri.Point(300, 100)
-  //];
-  var swctx = new poly2tri.SweepContext(simplifiedPoints);
+  var swctx = new poly2tri.SweepContext(simplifiedMergedPoints);
 
   swctx.triangulate();
   var triangles = swctx.getTriangles();
-
-  //console.log(triangles.length, triangles);
 
   var polygonPath = function(ctx, points) {
     ctx.beginPath();
@@ -232,51 +197,40 @@ function handleTestClick()
 
   // auto scale / translate
   bounds = swctx.getBoundingBox();
-  xscale = (context.canvas.width - 2 * MARGIN) / (bounds.max.x - bounds.min.x);
-  yscale = (context.canvas.height - 2 * MARGIN) / (bounds.max.y - bounds.min.y);
+  xscale = (contextTri.canvas.width - 2 * MARGIN) / (bounds.max.x - bounds.min.x);
+  yscale = (contextTri.canvas.height - 2 * MARGIN) / (bounds.max.y - bounds.min.y);
   scale = Math.min(xscale, yscale);
-  //scale = 1;
-  //context.translate(MARGIN, MARGIN);
-  context.scale(scale, scale);
-  context.translate(-bounds.min.x, -bounds.min.y);
+  contextTri.translate(MARGIN, MARGIN);
+  contextTri.scale(scale, scale);
+  contextTri.translate(-bounds.min.x, -bounds.min.y);
   linescale = 1 / scale;
-
-  // draw result
-  context.lineWidth = linescale;
-  context.fillStyle = TRIANGLE_FILL_STYLE;
-  context.strokeStyle = TRIANGLE_STROKE_STYLE;
-  //context.setLineDash(null);
-
-  triangles.forEach(function(t) {
-    polygonPath(context, [t.getPoint(0), t.getPoint(1), t.getPoint(2)]);
-    context.fill();
-    context.stroke();
-  });
 
   /*
   // draw constraints
-  context.lineWidth = 4 * linescale;
-  context.strokeStyle = CONSTRAINT_STYLE;
-  context.fillStyle = CONSTRAINT_STYLE;
-  //context.setLineDash([10 * linescale, 5 * linescale]);
-
-  polygonPath(context, contour);
-  context.stroke();
+  contextTri.lineWidth = 4 * linescale;
+  contextTri.strokeStyle = CONSTRAINT_STYLE;
+  contextTri.fillStyle = CONSTRAINT_STYLE;
+  polygonPath(contextTri, simplifiedMergedPoints);
+  contextTri.stroke();
   */
+
+  // draw result
+  contextTri.lineWidth = linescale;
+  contextTri.fillStyle = TRIANGLE_FILL_STYLE;
+  contextTri.strokeStyle = TRIANGLE_STROKE_STYLE;
+
+  triangles.forEach(function(t) {
+    polygonPath(contextTri, [t.getPoint(0), t.getPoint(1), t.getPoint(2)]);
+    contextTri.fill();
+    contextTri.stroke();
+  });
 
   //holes.forEach(function(hole) {
   //    polygonPath(ctx, hole);
   //    ctx.stroke();
   //});
 
-/*
-      points.forEach(function(point) {
-          ctx.beginPath();
-          ctx.arc(point.x, point.y, ctx.lineWidth, 0, 2 * Math.PI, false);
-          ctx.closePath();
-          ctx.fill();
-      });
-
+  /*
   // highlight errors, if any
   if (error_points) {
       ctx.lineWidth = 4 * linescale;
@@ -288,18 +242,12 @@ function handleTestClick()
           ctx.fill();
       });
   }
-*/
-
-
-
-
-
+  */
 }
 
 var step = function (timestamp) {
   var progress = timestamp - start;
   start = timestamp;
-
   //window.requestAnimationFrame(step);
 };
 
@@ -313,8 +261,13 @@ var main = function(ev) {
   canvasOut.width = canvasOut.offsetWidth;
   canvasOut.height = canvasOut.offsetHeight;
 
+  document.body.appendChild(canvasTri);
+  canvasTri.width = canvasTri.offsetWidth;
+  canvasTri.height = canvasTri.offsetHeight;
+
   context.fillStyle = 'red';
   context.fillRect(64, 64, 32, 32);
+  context.fillRect(80, 80, 32, 32);
 
   handleTestClick();
 
